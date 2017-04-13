dc030e5fc961060c75613b3254347c1855bfcd4e
Ann Gustafson <anngustafson@mymail.mines.edu>
2017-04-12 18:21:14 -0600

    Multi-sensors are now on timers

--- spaceBot/spaceBot.ino
+++ spaceBot/spaceBot.ino
@@ -14,13 +14,13 @@
 
 //Ultrasonic pins and max distance they can read
 #define SONAR_NUM 3 //Number of ultrasonics
-#PING_INTERVAL 33
-#define TRIGGER_PIN_FRONT 3
-#define ECHO_PIN_FRONT 2
+#define PING_INTERVAL 33 //33 ms per sensor
 #define TRIGGER_PIN_LEFT 7
 #define ECHO_PIN_LEFT 6
 #define TRIGGER_PIN_RIGHT 5
 #define ECHO_PIN_RIGHT 4
+#define TRIGGER_PIN_FRONT 3
+#define ECHO_PIN_FRONT 2
 #define MAX_DISTANCE 400
 
 //Magnometer Declination
@@ -29,14 +29,24 @@
 #define LSM9DS1_M  0x1E // Would be 0x1C if SDO_M is LOW
 #define LSM9DS1_AG  0x6B // Would be 0x6A if SDO_AG is LOW
 
-
 //Distance we want to alert robot
 #define HITTING_DISTANCE 15
 
+unsigned long pingTimer[SONAR_NUM]; //When each pings
+unsigned int cm[SONAR_NUM]; //Store ping distances
+uint8_t currentSensor = 0; //Which sensor is currently going
+
 //The left and right ultrasonics
-NewPing sonarLeft(TRIGGER_PIN_LEFT, ECHO_PIN_LEFT, MAX_DISTANCE);
-NewPing sonarRight(TRIGGER_PIN_RIGHT, ECHO_PIN_RIGHT, MAX_DISTANCE);
-NewPing sonarFront(TRIGGER_PIN_FRONT, ECHO_PIN_FRONT, MAX_DISTANCE);
+//NewPing sonarLeft(TRIGGER_PIN_LEFT, ECHO_PIN_LEFT, MAX_DISTANCE);
+//NewPing sonarRight(TRIGGER_PIN_RIGHT, ECHO_PIN_RIGHT, MAX_DISTANCE);
+//NewPing sonarFront(TRIGGER_PIN_FRONT, ECHO_PIN_FRONT, MAX_DISTANCE);
+
+//Array of sensors
+NewPing sonar[SONAR_NUM] = {
+    NewPing(TRIGGER_PIN_LEFT, ECHO_PIN_LEFT, MAX_DISTANCE),
+    NewPing(TRIGGER_PIN_RIGHT, ECHO_PIN_RIGHT, MAX_DISTANCE),
+    NewPing(TRIGGER_PIN_FRONT, ECHO_PIN_FRONT, MAX_DISTANCE)
+  };
 
 //Motor direction and PWM pins
 int motorLeftDir = 8;
@@ -55,6 +65,10 @@ void setup()
   pinMode(motorLeftPwm, OUTPUT);
   pinMode(motorRightPwm, OUTPUT);
 
+  pingTimer[0] = millis() + 75; // First ping start in ms.
+  for (uint8_t i = 1; i < SONAR_NUM; i++)
+    pingTimer[i] = pingTimer[i - 1] + PING_INTERVAL;
+
   imu.settings.device.commInterface = IMU_MODE_I2C;
   imu.settings.device.mAddress = LSM9DS1_M;
   imu.settings.device.agAddress = LSM9DS1_AG;
@@ -64,7 +78,7 @@ void loop()
 {
   delay(50);  //Not sure if we'll actually want the delay here
   //The distances from the left and right sensors
-  unsigned int distanceL = sonarLeft.ping_cm();
+ /* unsigned int distanceL = sonarLeft.ping_cm();
   unsigned int distanceR = sonarRight.ping_cm();
   
   //This will check how close we are to the alotted distance from the object and
@@ -76,7 +90,18 @@ void loop()
       distanceR = sonarRight.ping_cm();
       checkRightUltra(distanceR);
       distanceL = sonarLeft.ping_cm();
-  }
+  }*/
+  for (uint8_t i = 0; i < SONAR_NUM; i++) {
+    if (millis() >= pingTimer[i]) {
+      pingTimer[i] += PING_INTERVAL * SONAR_NUM;
+      if (i == 0 && currentSensor == SONAR_NUM - 1)
+        oneSensorCycle(); // Do something with results.
+      sonar[currentSensor].timer_stop();
+      currentSensor = i;
+      cm[currentSensor] = 0;
+      sonar[currentSensor].ping_timer(echoCheck);
+    }
+  
   forward();
 }
 
@@ -141,6 +166,19 @@ void setMotor(int motorPin,int dirPin, double power){
 
 
 //ULTRASONIC FUNCTIONS
+void echoCheck() { // If ping echo, set distance to array.
+  if (sonar[currentSensor].check_timer())
+    cm[currentSensor] = sonar[currentSensor].ping_result / US_ROUNDTRIP_CM;
+}
+ 
+void oneSensorCycle() { // Do something with the results.
+  for (uint8_t i = 0; i < SONAR_NUM; i++) {
+    Serial.print(i);
+    Serial.print("=");
+    Serial.print(cm[i]);
+    Serial.print("cm ");
+  }
+
 void checkLeftUltra(unsigned int dist)
 {
   while (dist <= HITTING_DISTANCE)
